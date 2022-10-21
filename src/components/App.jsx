00c47265e6e522084632cc3f76f895813554c215
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-21 16:36:45 +0300

    add urlkey in task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
+const URL = 'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
+const URL_KEY = '29840242-456192cfb7aa7dbb81a60ed73'
 export const App = () => {
+
   return (
     <div
       style={{
