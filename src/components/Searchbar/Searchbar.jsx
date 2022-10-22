53b0df76930e1cbf52250326ba7048740a3093e4
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-22 21:25:28 +0300

    add searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,29 @@
+import { Formik, Form, Field } from 'formik';
 
-export const SearchBar = ({ onSubmit}) => { 
+export const SearchBar = ({ onSubmit }) => { 
+  const handleSubmit = (name, page = 1, actions) => {
+    onSubmit(name, (page = 1));
+    actions.resetForm();
+  };
   return (
-<header class="searchbar">
-  <form class="form" onSubmit={name, page = 1}>
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+    <header class="searchbar">
+      <Formik initialValues={{image: ''}} onSubmit = {handleSubmit}>
+        <Form class="form" >
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>
-  )
+          <Field
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Formik>
+    </header>
+  );
 }
 
 
