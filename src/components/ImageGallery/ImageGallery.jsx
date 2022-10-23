305090725a5eb7e93f7d7a2f8d6a45195fee65cd
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-23 03:53:50 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,12 +2,16 @@ import { ImageGallery } from "components/ImageGalleryItem/ImageGalleryItem";
 
 export const ImageGalleryBox = ({ items }) => { 
   return (
-    <ul class="gallery">
-      {items.map((item) => (
-        <ImageGallery item={ item } />
-      ))}
-      
-    </ul>
+    <>
+      <ul class="gallery">
+        
+        {items.map(item => (
+          
+          <ImageGallery item={item} />
+          
+        ))}
+      </ul>
+    </>
   );
 }
 
