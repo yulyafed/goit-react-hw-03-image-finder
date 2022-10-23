0a3e19cf1e7bf8dfad88ce0bbfc29d7d485c019b
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-23 23:04:11 +0300

    add styles

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
  
 export const List = styled.ul`
     display: grid;
-    max-width: calc(100vw-48px);
+    max-width: calc(100vw - 48px);
     grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
     grid-gap: 16px;
     margin-top: 0;
